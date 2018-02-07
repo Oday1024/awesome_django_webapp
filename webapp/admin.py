e98ce68854cae156920ed3035488cd9c132ffce3
shenfeng <shf1024@vip.qq.com>
2018-02-07 17:30:00 +0800

    实现博客文章数据动态获取

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Users, Blogs, Comments
 
 # Register your models here.
+
+
+admin.site.register(Users)
+admin.site.register(Blogs)
+admin.site.register(Comments)
--- webapp/views.py
+++ webapp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Blogs, Comments, Users
+from .models import Users, Blogs, Comments
 
 # Create your views here.
 
 
 class IndexView(generic.ListView):
     template_name = 'index.html'
+    context_object_name = 'blog_list'
 
 
     def get_queryset(self):
-        return Users.objects.all()
+        return Blogs.objects.all()
